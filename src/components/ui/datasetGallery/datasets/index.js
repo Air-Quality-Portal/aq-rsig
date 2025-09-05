c60527da2be63c16434f6fde4fbcba5cf79059b0
Paridhi <073bct525.paridhi@pcampus.edu.np>
2025-09-05 14:33:25 -0500

    add more info about datasets

--- src/components/ui/datasetGallery/datasets/index.js
+++ src/components/ui/datasetGallery/datasets/index.js
@@ -5,14 +5,28 @@ export const galleryData = {
       name: 'OMI-2D',
       thumbnailUrl: '/path/to/thumbnail1.jpg',
       type: 'raster',
-      description: 'Ozone Monitoring Instrument NO2 data'
+      description: 'Berkeley High Resolution(BEHR) Ozone Monitoring Instrument (OMI) Integrated Column Amount of NO2 in the Tropopsphere.',
+      units: 'molec/cm²',
+      rescale_values : [0.1e+15, 6.4e+15],
+      colormap: 'viridis',
+      info: 'https://amt.copernicus.org/articles/14/455/2021/',
+      time_interval: 'daily',
+      start_date: '2014-06-01T00:00:00Z',
+      end_date: '2014-09-30T00:00:00Z'
     },
     {
       id: 'TROPESS_reanalysis_mon_emi_nox_anth',
-      name: 'TROPESS',
+      name: 'TROPESS Chemical Reanalysis Surface Anthropogenic NOx emissions Monthly 2-dimensional Product V1',
       type: 'netcdf-2d',
       thumbnailUrl: '/path/to/thumbnail3.jpg',
-      description: 'TROPESS reanalysis NOx emissions'
+      description: "The TROPESS Chemical Reanalysis Surface Total NOx emissions Monthly 2-dimensional Product contains nitrogen oxides (NO and NO2) emissions from the total of all sources. The data are part of the Tropospheric Chemical Reanalysis v2 (TCR-2) for the period 2005-2021. TCR-2 uses JPL's Multi-mOdel Multi-cOnstituent Chemical (MOMO-Chem) data assimilation framework that simultaneously optimizes both concentrations and emissions of multiple species from multiple satellite sensors. The data files are written in the netCDF version 4 file format, and each file contains a year of data at monthly resolution, and a spatial resolution of 1.125 x 1.125 degrees. The principal investigator for the TCR-2 data is Miyazaki, Kazuyuki.",
+      units: 'kg m-2 s-1',
+      rescale_values : [0, 4.786979e-10],
+      colormap: 'reds',
+      info: 'https://data.nasa.gov/dataset/tropess-chemical-reanalysis-surface-total-nox-emissions-monthly-2-dimensional-product-v1-t-2206f',
+      time_interval: 'monthly',
+      start_date: '2005-01-01T00:00:00+00:00',
+      end_date: '2021-12-31T00:00:00+00:00'
     }
   ],
   'insitu': [
@@ -21,7 +35,11 @@ export const galleryData = {
       name: 'AQS Stations',
       type: 'feature',
       thumbnailUrl: '/path/to/thumbnail1.jpg',
-      description: 'Air Quality System monitoring stations'
+      description: 'Air Quality System monitoring stations',
+      info: 'https://data.nasa.gov/dataset/tropess-chemical-reanalysis-surface-total-nox-emissions-monthly-2-dimensional-product-v1-t-2206f',
+      time_interval: 'yearly',
+      start_date: '2017-01-01T00:00:00+00:00',
+      end_date: '2020-12-31T00:00:00+00:00'
     }
   ],
   'Lidar': [
